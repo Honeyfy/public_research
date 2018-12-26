26b4d2bb5ebf251243ce34438cea0740441989a2
GolanLevy <39908864+GolanLevy@users.noreply.github.com>
2018-12-26 14:39:54 +0200

    Fix ID in call page and Team Members page

--- bookmarklets/showIds.js
+++ bookmarklets/showIds.js
@@ -2,7 +2,7 @@ javascript: (function () {
   $("a[data-call-id]").each(function () {
     $(this).parent().append("<span style='color:blue;padding-left:10px'>" + $(this).attr("data-call-id") + "</span>")
   });
-  $("tr.user").find("td:first a").each(function () {
+  $("div.user").find("a:first").each(function () {
     $(this).parent().append("<span style='color:blue;padding-left:8px'>" + $(this).attr("href").split("user-id=")[1] + "</span>");
   });
   $("div.company .name a").each(function () {
@@ -13,6 +13,6 @@ javascript: (function () {
     $(this).prepend("<span style='color:blue;padding-left:10px'>" + $(this).attr("data-speaker-id") + "</span>");
   });
   $("body#call h1").eq(0).each(function () {
-    $(this).append("<span style='color:white;padding-left:10px;font-size:12px'>company: " + data.call.companyId + " owner: " + data.owner.appUserID + "</span>");
+    $(this).append("<span style='color:blue;padding-left:10px;font-size:12px'>company: " + data.call.companyId + " owner: " + data.owner.appUserID + "</span>");
   });
 })()
